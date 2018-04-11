4f8f0fd689040483c78c2d4fc100c300d73e08e7
Miguel Angel <miguel.osorio@maspm.lat>
2018-04-11 11:05:30 -0500

    Actualizacion del proyecto se agregaon los tipos de metodos enumerados, asi como las funciones de validación. If, when

--- app/src/main/java/com/example/miguelangelosoriocruz/kotlincourse/MediaItem.kt
+++ app/src/main/java/com/example/miguelangelosoriocruz/kotlincourse/MediaItem.kt
@@ -1,5 +1,34 @@
 package com.example.miguelangelosoriocruz.kotlincourse
 
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
 data class MediaItem(val title: String, val urlImage: String, val type: Type) {
     enum class Type {PHOTO, VIDEO}
+}
+
+fun test(type: MediaItem.Type) {
+    when(type) {
+        MediaItem.Type.PHOTO -> print("es de tipo foto")
+        MediaItem.Type.VIDEO -> print("es una foto")
+    }
+}
+
+fun test2(view: View) {
+    when(view) {
+        is TextView -> view.text = "asopotamdre"
+        is EditText -> view.setText("")
+
+    }
+}
+
+fun test3(number: String) : Int{
+    val response: Int = when(number) {
+        "que pacho" -> 1
+        "hello world" -> 2
+        else -> 0
+    }
+
+    return response
 }
